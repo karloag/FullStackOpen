063eb2ec69d9f4b4737b1bb5f5445ad5d55f113d
Karlo <karlo.alcaraz17@tectijuana.edu.mx>
2025-10-28 21:10:34 -0700

    2.4

--- Part2/course-info-step-6/src/App.jsx
+++ Part2/course-info-step-6/src/App.jsx
@@ -1,33 +1,56 @@
 /* eslint-disable react/prop-types */
 const App = () => {
-  const course = {
-   id : 1,
+  const courses = [ {
    name : "Half Stack application deve lopment" ,
+   id: 1,
    parts : [ 
-    
     {
      name: 'Fundamentals of React',
      exercises : 10,
      id: 1
     },
-   
     {
      name: 'Using props to pass data',  
      exercises:  7,
      id:2
     },
-  
     {
       name: 'State of a component',
       exercises: 14,
       id: 3
-    }  
+    },
+    {
+      name:'Redux',
+      exercises: 11,
+      id:4
+    } 
   ]
-}
+},
+
+{ 
+  name: 'Node.js',
+  id: 2,
+  parts:[
+    {
+      name:'Routing',
+      exercises: 3,
+      id: 1
+    },
+    {
+      name: 'Middlewares',
+      exercises: 7,
+      id: 2
+    }
+  ] //parts
+ } //2nd course 
+] //all courses
 
   return (
     <div> 
-      <Course course={course}/>
+      <h1>Web development curriculum</h1>
+      {courses.map(course=>
+        <Course key={course.id} course={course} /> 
+      )}
     </div>
   )
 }
