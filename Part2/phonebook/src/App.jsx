3cbcf0098486af16e55f792f72c14974bffe9136
Karlo <karlo.alcaraz17@tectijuana.edu.mx>
2025-11-17 23:54:51 -0800

    2.6 step one (I shouldnt have commited the previous one)

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -6,19 +6,32 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) =>{ 
+    setNewName(event.target.value)
+    console.log(event.target.value)
+  }
+  const addPerson = (event) =>{
+    event.preventDefault()
+    const personObject = { name: newName }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
   return(
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input></input>
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person, i ) => <li key={i}>{person.name}</li>)}
+      </ul>
+      
     </div>
   )
 }
